a7d3c85f2947cb9d35ca6888cdb4658f01615331
rhksgmz <boh060830@gmail.com>
2026-07-22 21:13:57 +0800

    Update script.js

--- script.js
+++ script.js
@@ -123,9 +123,9 @@ document.addEventListener('DOMContentLoaded', () => {
     },
     3: {
       positions: [
-        { left: 15, top: 17, width: 210, height: 183 },   // 第一格
+        { left: 15, top: 19, width: 210, height: 183 },   // 第一格
         { left: 15, top: 212, width: 210, height: 183 },  // 第二格 (間隔 0.35cm = 13px)
-        { left: 15, top: 396, width: 210, height: 183 }   // 第三格 (間隔 0.35cm = 13px)
+        { left: 15, top: 404, width: 210, height: 183 }   // 第三格 (間隔 0.35cm = 13px)
       ]
     }
   };
